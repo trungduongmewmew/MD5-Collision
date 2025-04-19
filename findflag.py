15af0597a17f51e8d46388ad16d028b2efb9a1d1
trungduongmewmew <duongtrungduongmew@gmail.com>
2025-04-19 14:47:44 +0700

    modified:   findflag.py

--- findflag.py
+++ findflag.py
@@ -2,8 +2,6 @@ import hashlib
 import requests
 string1 = "TEXTCOLLBYfGiJUETHQ4hAcKSMd5zYpgqf1YRDhkmxHkhPWptrkoyz28wnI9V0aHeAuaKnak"
 string2 = "TEXTCOLLBYfGiJUETHQ4hEcKSMd5zYpgqf1YRDhkmxHkhPWptrkoyz28wnI9V0aHeAuaKnak"
-def calculate_md5(s):
-    return hashlib.md5(s.encode()).hexdigest()
 url = 'http://172.104.49.143:1608/'  
 data = {
     'a': string1,
